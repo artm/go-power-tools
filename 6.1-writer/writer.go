78d104a95362dcc45fcd57d41d4d2776d0251902
Artem Baguinski <a.baguinski@performation.com>
2021-12-15 20:31:43 +0100

    Split off cli into its own file

--- 6.1-writer/writer.go
+++ 6.1-writer/writer.go
@@ -1,17 +1,9 @@
 package writer
 
 import (
-	"flag"
 	"fmt"
 	"os"
 )
-
-type cli struct {
-	path string
-	size int
-}
-
-type option func(*cli) error
 
 const bufferSize = 10000
 
@@ -43,45 +35,6 @@ func WriteZeros(path string, size int) error {
 		size -= chunkSize
 	}
 	return nil
-}
-
-func RunCLI() {
-	cli, err := NewCLI(
-		FromArgs(os.Args[1:]),
-	)
-	checkErr(err)
-	err = cli.Write()
-	checkErr(err)
-}
-
-func NewCLI(options ...option) (*cli, error) {
-	cli := &cli{}
-	for _, opt := range options {
-		err := opt(cli)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return cli, nil
-}
-
-func FromArgs(args []string) option {
-	return func(cli *cli) error {
-		fset := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
-		fset.IntVar(&cli.size, "size", 1000, "number of zeros to write")
-		err := fset.Parse(args)
-		checkErr(err)
-		rest := fset.Args()
-		if len(rest) != 1 {
-			checkErr(fmt.Errorf("single filename argument is required"))
-		}
-		cli.path = rest[0]
-		return nil
-	}
-}
-
-func (cli *cli) Write() error {
-	return WriteZeros(cli.path, cli.size)
 }
 
 func checkErr(err error) {
